47e788f918474e16d64fdbdf16e6b310d4fa87b1
igoorodrigues <igoor.rodriguess@outlook.com>
2021-09-16 20:10:32 -0300

    Creating employees models

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+    department = models.CharField(max_length=255)
+
