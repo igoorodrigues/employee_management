663a6c47de1f83ad08541352a5167c5dbf0a538e
igoorodrigues <igoor.rodriguess@outlook.com>
2021-09-16 20:35:30 -0300

    Developing the employee template

--- portal/views.py
+++ portal/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from portal.models import Employee
+
+
+def employee(request):
+    employees = Employee.objects.all()
+
+    context = {
+        'employess': employees
+    }
+
+    return render(request, 'portal/employee.html', context=context)
